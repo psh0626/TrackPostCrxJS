f62851d300fa0d6a12d66c809a3cff927fec0af8
Sunghoon Park <dodgedog4@gmail.com>
2024-03-18 17:56:29 +0900

    ff

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <Button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </Button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Stack spacing={2} margin={20}>
+      <TextField variant="outlined"  />
+      
+    </Stack>
   )
 }
 
