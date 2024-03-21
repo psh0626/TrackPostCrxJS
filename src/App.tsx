11af89ba4bf1528f44e1b616865570b2ce51f04c
Sunghoon Park <dodgedog4@gmail.com>
2024-03-22 01:25:16 +0900

    d

--- src/App.tsx
+++ src/App.tsx
@@ -11,30 +11,60 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
+class XMLUtil {
+  private xmlDocument: Document;
+
+  constructor(xmlDocument: Document) {
+    this.xmlDocument = xmlDocument;
+  }
+
+  getData(name: string): string {
+    return this.xmlDocument.querySelector(name)?.textContent ?? "";
+  }
+}
 function App() {
   const [count, setCount] = useState(0);
   const postURL = "https://ems.epost.go.kr/trace.RegisterEmsClaimAjax.postal";
 
   var boundText: string = "";
-
-  const FetchPostItem = () => fetch(postURL, {
+  const FetchPostItem = () =>
+    fetch(postURL, {
       method: "POST",
-      headers: { 
-        'Content-Type': 'application/xml; charset=utf-8'
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
       },
-      body: `<POST_CODE>${boundText}</POST_CODE>`
-    }).then((response) => {
-      alert(response.text);
-    });
+      body: `POST_CODE=${boundText}`,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log(data);
+        const parser = new DOMParser();
+        const xmlDoc = new XMLUtil(parser.parseFromString(data, "text/xml"));
+
+        console.log(xmlDoc.getData("MAIL_NO"));
+      });
   return (
     <Stack spacing={2} margin={5}>
-      <TextField variant="outlined" label="EB123456789KR" onChange={(e) => {boundText = e.target.value}} />
-      <Button variant="contained"
-        onClick={()=>{
-          alert(boundText);
+      <TextField
+        variant="outlined"
+        label="EB123456789KR"
+        onChange={(e) => {
+          boundText = e.target.value.toUpperCase();
+        }}
+      />
+      <Button
+        variant="contained"
+        onClick={() => {
           FetchPostItem();
         }}
-      >SEARCH</Button>
+      >
+        SEARCH
+      </Button>
       <Divider classes={"ma-10"} />
       <Card>
         <Stack spacing={0.5}>
