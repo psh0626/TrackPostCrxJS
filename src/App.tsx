cc143bf77d58e86fe0af2850dc19a83422da41d2
Sunghoon Park <dodgedog4@gmail.com>
2024-03-21 14:49:39 +0900

    ggg

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,54 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { useState } from "react";
+import logo from "./logo.svg";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <Stack spacing={2} margin={20}>
-      <TextField variant="outlined"  />
-      
+    <Stack spacing={2} margin={5}>
+      <TextField variant="outlined" />
+      <Divider />
+      <Card>
+        <Stack spacing={0.5}>
+          <Accordion disableGutters>
+            <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+              <Typography variant="h6">Sender</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack spacing={2}>
+                <TextField variant="outlined" />
+                <TextField variant="outlined" />
+                <TextField variant="outlined" />
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+
+          <Accordion disableGutters>
+            <AccordionSummary expandIcon={<ArrowDropDownIcon />}>
+              <Typography variant="h6">Addressee</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack spacing={2}>
+                <TextField variant="outlined" />
+                <TextField variant="outlined" />
+                <TextField variant="outlined" />
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+        </Stack>
+      </Card>
     </Stack>
-  )
+  );
 }
 
-export default App
+export default App;
