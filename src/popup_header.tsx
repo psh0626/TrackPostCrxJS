cfeb6a79f8e24cb709f259c603a45e0b0fe6bfeb
Sunghoon Park <dodgedog4@gmail.com>
2024-04-04 00:52:59 +0900

    import fix

--- src/popup_header.tsx
+++ src/popup_header.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { AppBar, colors, Stack, Typography, Toolbar, TabPanel } from "@mui/material";
+import { AppBar, colors, Stack, Typography, Toolbar } from "@mui/material";
 import { ReactNode } from "react";
 import { TravelExplore } from "@mui/icons-material";
-interface nested_component{
+interface nested_component {
   children?: ReactNode;
 }
 export default function PopupHeader({ children }: nested_component) {
@@ -10,7 +10,7 @@ export default function PopupHeader({ children }: nested_component) {
     <Stack color={colors.deepOrange} marginTop={5} marginBottom={5}>
       <AppBar position="fixed" color="primary">
         <Toolbar>
-          <TravelExplore sx={{mr: 1}}/>
+          <TravelExplore sx={{ mr: 1 }} />
           <Typography variant="h6" textAlign="center" fontWeight={300}>
             국제우편 행방조사
           </Typography>
