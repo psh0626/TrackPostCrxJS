db5bbc4065d68c02198b32d9d6da98322efdad25
Sunghoon Park <dodgedog4@gmail.com>
2024-10-22 01:31:19 +0900

    post build change

--- post-build-zip.ps1
+++ post-build-zip.ps1
@@ -14,12 +14,12 @@ Write-Output "Creating dist.zip..."
 Compress-Archive -Path "dist\\*" -DestinationPath "publish\\dist.zip" -Force
 Write-Output "dist.zip file has been created!"
 
-Set-Location publish
-git add dist.zip
-git commit
-git push origin main
+# Set-Location publish
+# git add dist.zip
+# git commit
+# git push origin main
 
-Set-Location ..
-git add publish
-git commit -m "Update submodule reference"
-git push origin main
+# Set-Location ..
+# git add publish
+# git commit -m "Update submodule reference"
+# git push origin main
