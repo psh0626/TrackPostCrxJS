9a26c3fe8eb3786b00fdb6818c7a0abd8de95e5c
Sunghoon Park <dodgedog4@gmail.com>
2024-10-22 01:00:39 +0900

    test 1

--- post-build-zip.ps1
+++ post-build-zip.ps1
@@ -1,11 +1,34 @@
 
+$directoryPath = ".\publish"
+
+# Check if the directory already exists
+if (-not (Test-Path -Path $directoryPath)) {
+    # If the directory does not exist, create it
+    mkdir $directoryPath
+    Write-Output "Directory created: $directoryPath"
+} else {
+    Write-Output "Directory already exists: $directoryPath"
+}
+
 Write-Output "Creating dist.zip..."
-Compress-Archive -Path "dist\\*" -DestinationPath "dist.zip" -Force
+Compress-Archive -Path "dist\\*" -DestinationPath "publish\\dist.zip" -Force
 Write-Output "dist.zip file has been created!"
 
-# Write-Output "Pushing Dist.zip to the Repo: TrackPostExtZip"
-# git checkout --orphan dist_branch
-# git reset
-# git add .\dist.zip
-# git commit -m "Update dist.zip"
-# git push -f dist main
+# try{
+# git remote add publish_dist https://github.com/shawnpark9494/TrackPostExtZip.git
+# }catch{
+#   exit;
+# }
+# git add publish/dist.zip
+
+# Write-Output "Creating commit..."
+# git commit -m "Add build artifacts"
+
+# # Replace 'your-remote' with the desired remote name and 'your-branch' with the desired branch name
+# $remote = "publish_dist"
+# $branch = "main"
+
+# Write-Output "Pushing changes to remote..."
+# git push $remote $branch --force
+
+# git remote rm publish_dist
