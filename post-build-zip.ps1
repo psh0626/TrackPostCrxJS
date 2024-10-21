ce6ca55b4cb6d8f4306c3564044b6c17b9bb6ac1
Sunghoon Park <dodgedog4@gmail.com>
2024-10-21 22:00:55 +0900

    test commit2

--- post-build-zip.ps1
+++ post-build-zip.ps1
@@ -1,6 +1,7 @@
 
 Write-Output "Creating dist.zip..."
 Compress-Archive -Path "dist\\*" -DestinationPath "dist.zip" -Force
+Write-Output "dist.zip file has been created!"
 
 # Write-Output "Pushing Dist.zip to the Repo: TrackPostExtZip"
 # git checkout --orphan dist_branch
