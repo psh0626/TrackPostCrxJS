1b5e4b518a7758a892c5bed9b004e62e8424efe5
Dopa at Company <dodgedgo4@gmail.com>
2025-01-07 15:37:44 +0900

    - input kr2en enhancement

--- src/background/injectDOM/InjectUtil.tsx
+++ src/background/injectDOM/InjectUtil.tsx
@@ -52,41 +52,98 @@ class InjectUtil {
             target_elm
         );
     }
-    private static kor2en(text: string) {
-        const mapping: { [key: string]: string } = {
-            ㅂ: "q",
-            ㅈ: "w",
-            ㄷ: "e",
-            ㄱ: "r",
-            ㅅ: "t",
-            ㅛ: "y",
-            ㅕ: "u",
-            ㅑ: "i",
-            ㅐ: "o",
-            ㅔ: "p",
-            ㅁ: "a",
-            ㄴ: "s",
-            ㅇ: "d",
-            ㄹ: "f",
-            ㅎ: "g",
-            ㅗ: "h",
-            ㅓ: "j",
-            ㅏ: "k",
-            ㅣ: "l",
-            ㅋ: "z",
-            ㅌ: "x",
-            ㅊ: "c",
-            ㅍ: "v",
-            ㅠ: "b",
-            ㅜ: "n",
-            ㅡ: "m",
-        };
 
-        return text
-            .split("")
-            .map((char) => mapping[char] || char)
-            .join("");
+    private static kr2en(text: string): string {
+        const jamoMap = new Map<string, string>([
+            ["ㄱ", "r"],
+            ["ㄲ", "R"],
+            ["ㄴ", "s"],
+            ["ㄷ", "e"],
+            ["ㄸ", "E"],
+            ["ㄹ", "f"],
+            ["ㅁ", "a"],
+            ["ㅂ", "q"],
+            ["ㅃ", "Q"],
+            ["ㅅ", "t"],
+            ["ㅆ", "T"],
+            ["ㅇ", "d"],
+            ["ㅈ", "w"],
+            ["ㅉ", "W"],
+            ["ㅊ", "c"],
+            ["ㅋ", "z"],
+            ["ㅌ", "x"],
+            ["ㅍ", "v"],
+            ["ㅎ", "g"],
+            ["ㅏ", "k"],
+            ["ㅐ", "o"],
+            ["ㅑ", "i"],
+            ["ㅒ", "O"],
+            ["ㅓ", "j"],
+            ["ㅔ", "p"],
+            ["ㅕ", "u"],
+            ["ㅖ", "P"],
+            ["ㅗ", "h"],
+            ["ㅘ", "hk"],
+            ["ㅙ", "ho"],
+            ["ㅚ", "hl"],
+            ["ㅛ", "y"],
+            ["ㅜ", "n"],
+            ["ㅝ", "nj"],
+            ["ㅞ", "np"],
+            ["ㅟ", "nl"],
+            ["ㅠ", "b"],
+            ["ㅡ", "m"],
+            ["ㅢ", "ml"],
+            ["ㅣ", "l"],
+        ]);
+
+        function separateKoreanSyllable(syllable: string): string {
+            // console.log("St: " + syllable);
+            const charCode = syllable.charCodeAt(0);
+            if (charCode < 0xac00 || charCode > 0xd7a3) {
+                // console.log("Denied: " + syllable);
+                return syllable; // Return as is if it's not a Korean syllable
+            }
+
+            const jamoSet = [
+                "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ",
+                "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ",
+                " ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ",
+            ];
+
+            let offset = charCode - 0xac00;
+            const jong = offset % 28;
+            offset = (offset - jong) / 28;
+            const jung = offset % 21;
+            const cho = (offset - jung) / 21;
+
+            let result = jamoSet[0][cho] + jamoSet[1][jung];
+            if (jong > 0) {
+                result += jamoSet[2][jong];
+            }
+
+            console.log("Result: " + result);
+            return result[result.length - 1];
+        }
+
+        try {
+            return text
+                .split("")
+                .map((char) => {
+                    const separated = separateKoreanSyllable(char);
+                    const result = separated
+                        .split("")
+                        .map((jamo) => jamoMap.get(jamo) || jamo)
+                        .join("");
+                    // console.log("separated: " + result);
+                    return result;
+                })
+                .join("");
+        } catch {
+            return text;
+        }
     }
+
     static ChangeAttributes(
         target_input: HTMLInputElement,
         target_form: HTMLFormElement,
@@ -111,7 +168,8 @@ class InjectUtil {
         target_input.addEventListener("input", (e) => {
             const old_position = target_input.selectionStart;
             const input = e.target as HTMLInputElement;
-            const value = this.kor2en(input.value).toUpperCase();
+            const value = this.kr2en(input.value).toUpperCase();
+            // console.log("value: " + value);
 
             if (apply_pattern) {
                 target_input.setAttribute("maxlength", "13");
@@ -135,6 +193,7 @@ class InjectUtil {
             }
 
             if (input.selectionStart !== old_position) {
+                // console.log("position set from " + input.selectionStart + " to " + old_position);
                 input.setSelectionRange(old_position, old_position);
             }
         });
