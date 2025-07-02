a3b34d41cc4db3bc8bd0005ac7e5f53af454e564
Sunghoon Park <dodgedog4@gmail.com>
2025-07-02 15:19:13 +0900

    inject functionality update

--- src/background/GetUnreadReplies/GcssSum.ts
+++ src/background/GetUnreadReplies/GcssSum.ts
@@ -1,6 +1,5 @@
-const table = document.querySelector("table");
-const test = table ? [...table.rows].map((el) => el.cells[0].innerText).filter((el) => el.includes("E")) : [];
-const getQuery = async (itemId: string) => {
+
+async function getQuery(itemId: string) {
     const response = await fetch("https://gcss.ipc.be/CSS/gcss/query", {
         headers: {
             accept: "application/json, text/plain, */*",
@@ -17,17 +16,30 @@ const getQuery = async (itemId: string) => {
         .filter((el: { workflowType: string }) => el.workflowType.includes("Q"))
         .map((el: { author: string }) => el.author);
     return Qs;
-};
-const promiseAllQueries = test.map((el) => getQuery(el));
-const result = await Promise.all(promiseAllQueries);
-console.trace("DONE", result);
-
-const newHeader = document.createElement("th");
-newHeader.textContent = "last author";
-const beforeNode = document.querySelector("th[data-property='workflowType'");
-
-const headerRow = document.querySelector("tr.header");
-if (headerRow) {
-    headerRow.insertBefore(newHeader, beforeNode);
 }
+
+async function addAuthorColumn() {
+    const table = document.querySelector("table");
+    const test = table ? [...table.rows].map((el) => el.cells[0].innerText).filter((el) => el.includes("E")) : []; // TODO: change this to collect IDs from rows.   
+    const promiseAllQueries = test.map((el) => getQuery(el));
+    const result = await Promise.all(promiseAllQueries); // TODO: use allSettled for error handling
+    console.log(result);
+
+    let beforeNode = document.querySelector("th[data-property='workflowType']")!;
+    const newThHeader = beforeNode.cloneNode(true) as HTMLElement;
+    newThHeader.setAttribute("data-property", "lastAuthor");
+    newThHeader.textContent = "last req. author";
+    document.querySelector("tr.header")!.insertBefore(newThHeader, beforeNode);
+
+    beforeNode = document.querySelector("tr.filter")!.children[0];
+    const newThFilter = beforeNode.cloneNode(true);
+    document.querySelector("tr.filter")!.insertBefore(newThFilter, beforeNode);
+
+    [...document.querySelector("tbody")!.children].forEach((tr, i)=>{
+        const newTd = document.createElement("td");
+        newTd.textContent = result[i];
+        tr.insertBefore(newTd, tr.children[1]);
+    });
+}
+
 export {};
