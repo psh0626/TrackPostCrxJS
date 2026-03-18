72913ea0db405cc7819acd8e3c950f61a75b4327
Sunghoon Park <dodgedog4@gmail.com>
2026-03-18 22:39:45 +0900

    new gcss workflows done todo notifications

--- src/background/pending-replies/newGcssReplies.ts
+++ src/background/pending-replies/newGcssReplies.ts
@@ -1,4 +1,5 @@
 import { IMICSettings } from "../../lib/IMICSettings";
+import { COMMANDS, MSG } from "../../lib/Message";
 import { GcssResponse, GCSSWorkflow } from "./newGcssWrapper";
 
 type ViewType = "TODOS" | "REQUEST_SENT" | "REPLY_SENT" | "REPLY_RECEIVED";
@@ -33,30 +34,77 @@ export class GcssWorkflowService {
     private static client = new GcssWorkflowClient();
     static settings = new IMICSettings();
     static async fetchWorkflows() {
-        if (this.settings.GcssUnreadReplies) {
-            const response = await this.client.getWorkflows("REPLY_RECEIVED");
-            console.log("GCSS workflows fetched: ", response);
-            if (response.totalResults < 1) {
-                console.log("No data found.");
-                return;
-            }
+        const messages = [];
+        if (this.settings.GcssUnreadRequests) {
+            const workflows = await this.getResponseData("TODOS");
+            console.log("Filtering TODOS workflows with settings", this.settings.GcssRequestServiceTypes);
 
-            console.log("Filtering workflows with settings", this.settings.GcssAuthor, this.settings.GcssServiceTypes);
-
-            const filteredWorkflows = response.data.filter(
-                (workflow) =>
-                    this.includesOneOf(workflow.inquiryAuthorName, this.settings.GcssAuthor) &&
-                    this.includesOneOf(workflow.product, this.settings.GcssServiceTypes),
+            const filteredWorkflows = this.filterByUserSettings(workflows, (wf) =>
+                this.includesOneOf(wf.product, this.settings.GcssServiceTypes),
             );
-            console.log("Filtered workflows according to settings", filteredWorkflows);
 
-            const unreadWorkflows = filteredWorkflows.filter((workflow) => workflow.readStatus === "UNREAD");
-            console.log("Filtered unread workflows", unreadWorkflows);
+            const unreadWorkflows = this.filterUnread(filteredWorkflows);
 
-            await this.notifyMessageHub(filteredWorkflows);
+            if (unreadWorkflows) messages.push(this.notifyMessageHub(COMMANDS.GCSS_UNREAD_REQUESTS, unreadWorkflows));
+        }
+
+        if (this.settings.GcssUnreadReplies) {
+            const workflows = await this.getResponseData("REPLY_RECEIVED");
+            console.log(
+                "Filtering REPLY_RECEIVED workflows with settings",
+                this.settings.GcssAuthor,
+                this.settings.GcssServiceTypes,
+            );
+
+            const filteredWorkflows = this.filterByUserSettings(workflows, [
+                (wf) => this.includesOneOf(wf.inquiryAuthorName, this.settings.GcssAuthor),
+                (wf) => this.includesOneOf(wf.product, this.settings.GcssServiceTypes),
+            ]);
+
+            const unreadWorkflows = this.filterUnread(filteredWorkflows);
+
+            if (unreadWorkflows) messages.push(this.notifyMessageHub(COMMANDS.GCSS_UNREAD_REPLIES, unreadWorkflows));
+        }
+
+        if (this.settings.GcssUnreadNotificationInbound) {
+        }
+
+        if (this.settings.GcssUnreadNotificationOutbound) {
+        }
+
+        if (messages.length > 0) {
+            await Promise.all(messages);
         }
     }
-    static async notifyMessageHub(workflows: GCSSWorkflow[]) {}
+
+    static filterUnread(workflows: GCSSWorkflow[] | undefined) {
+        const filtered = workflows?.filter((workflow) => workflow.readStatus === "UNREAD");
+        console.log("Filtered unread workflows", filtered);
+        return filtered;
+    }
+
+    static filterByUserSettings(
+        responseData: GCSSWorkflow[] | null,
+        criteria: ((workflow: GCSSWorkflow) => boolean) | Array<(workflow: GCSSWorkflow) => boolean>,
+    ) {
+        const criteriaArray = Array.isArray(criteria) ? criteria : [criteria];
+        const filteredWorkflows = responseData?.filter((wf) => criteriaArray.every((func) => func(wf)));
+        console.log("Filtered workflows according to settings", filteredWorkflows);
+        return filteredWorkflows;
+    }
+    static async getResponseData(viewType: ViewType) {
+        await this.client.getWorkflows("REPLY_RECEIVED");
+        const response = await this.client.getWorkflows("REPLY_RECEIVED");
+        console.log("GCSS workflows fetched: ", response);
+        if (response.totalResults < 1) {
+            console.log("No data found.");
+            return null;
+        }
+        return response.data;
+    }
+    static async notifyMessageHub(messageType: COMMANDS, workflows: GCSSWorkflow[]) {
+        return chrome.runtime.sendMessage(new MSG(messageType, workflows));
+    }
     static includesOneOf(target: string, options: string[]) {
         return options.some((option) => target.toLowerCase().includes(option.toLowerCase()));
     }
