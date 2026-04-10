f45b4ea1b4b8156835690ec315c06a240d1b4cec
Dopa at Company <dodgedgo4@gmail.com>
2026-04-10 11:38:04 +0900

    remove dependency partifcles.js added dependency tsparticles / triangle presets

--- src/background-service/ui/app/tabs/about/about2.tsx
+++ src/background-service/ui/app/tabs/about/about2.tsx
@@ -1,5 +1,21 @@
+import { loadTrianglesPreset } from "@tsparticles/preset-triangles";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useState } from "react";
 import "./about2.css";
 
 export default function About2() {
-    return <div id="particles-container"></div>;
+    const [init, setInit] = useState(false);
+    useEffect(() => {
+        initParticlesEngine(async (engine) => {
+            await loadTrianglesPreset(engine);
+        }).then((_) => setInit(true));
+    }, []);
+
+    if (init) {
+        return <Particles id="particles" options={{
+            preset: "triangles", fullScreen: { enable: false },
+            
+        }} />;
+    }
+    return <></>;
 }
